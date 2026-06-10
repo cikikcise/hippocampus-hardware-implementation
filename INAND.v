ea2c226195c9ef1a5e3e390cddbb922dc5da38d1
cikikcise <sevde.cikikci@gmail.com>
2026-06-10 14:34:59 +0300

    Create INAND.v

---
+++ INAND.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+module INAND(
+    input wire clk, rst,
+    input wire signed [19:0] g1, g2, g3, g4, g5, g6, g7, g8,
+    output reg signed [19:0] g_out
+    );
+    
+    wire  signed [19:0] g_total;
+    Adder_Tree_8in adder_inst (
+        .clk(clk), .rst(rst),
+        .g1(g1), .g2(g2), .g3(g3), .g4(g4),
+        .g5(g5), .g6(g6), .g7(g7), .g8(g8),
+        .g_total(g_total)
+    );
+    
+    // KODDA 30mS 'de 23 aldığı için aynı orandan 30 aldım 30*8'den 985 çıkıyor
+    parameter signed [19:0] THRESHOLD = 20'sd985; // 30mS*8 
+    parameter signed [19:0] OUT_VAL   = 20'sd164; // 40 mS
+    
+    reg firing;
+    reg [3:0] counter;
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            firing <= 0;
+            counter <= 0;
+            g_out <= 0;
+        end else begin
+            // Ateşleme Mantığı
+            if (!firing) begin
+                if (g_total >= THRESHOLD) begin
+                    firing <= 1'b1;
+                    counter <= 0;
+                end
+            end else begin
+                // Counter Mantığı
+                if (counter >= 5) begin
+                    firing <= 1'b0;
+                    counter <= 0;
+                end else begin
+                    counter <= counter + 1;
+                end
+            end
+
+            if (firing && counter >= 1 && counter <= 3)
+                g_out <= OUT_VAL;
+            else
+                g_out <= 0;
+        end
+    end
+endmodule
